e0451c5ba6e4cfef2ecb3e3c9e6621863e894513
leor <leo442183205@gmail.com>
2021-03-12 17:18:13 -0800

    Define basic configurations for frame

---
+++ src/main/java/UI.java
@@ -0,0 +1,33 @@
+package main.java;
+
+import java.awt.*;
+
+public class UI extends Frame {
+    // **************** Variable declarations *********************//
+    int width, height;
+
+    // **************** End of Variable declarations **************//
+
+    //  Constructor for user interface
+    public UI() {
+        // Set the basic configurations of the frame
+        this.setTitle("IAT455 Course Project Group 1");
+        this.setVisible(true);
+
+        // Get single width  and height of the image
+        width = 500;
+        height = 500;
+    }
+
+    // The render method for the user interface
+    public void paint(Graphics g) {
+        this.setSize(1400, 1000);
+    }
+
+    //  Main method required to initialize the app
+    public static void main(String[] args) {
+        UI UI = new UI();// instantiate the class
+        UI.repaint(); // paint the frame
+
+    }
+}
