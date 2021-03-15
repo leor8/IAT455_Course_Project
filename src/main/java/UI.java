3c020a69923d7b44128062ecd2f3e8e6ca851d3e
leor <leo442183205@gmail.com>
2021-03-15 16:39:35 -0700

    Include a test image and video for testing purposes and display the  image at the top left corner

--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -1,10 +1,14 @@
 package main.java;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 public class UI extends Frame {
     // **************** Variable declarations *********************//
     int width, height;
+    BufferedImage testImage;
 
     // **************** End of Variable declarations **************//
 
@@ -14,14 +18,22 @@ public class UI extends Frame {
         this.setTitle("IAT455 Course Project Group 1");
         this.setVisible(true);
 
-        // Get single width  and height of the image
-        width = 500;
-        height = 500;
+        // Load test image and video
+        try{
+            testImage = ImageIO.read(new File("testImage.jpg"));
+            width = testImage.getWidth()/3;
+            height = testImage.getHeight()/3;
+        } catch (Exception e) {
+            System.out.println("Error loading the images: " + e);
+        }
     }
 
     // The render method for the user interface
     public void paint(Graphics g) {
         this.setSize(1400, 1000);
+
+        // Display test Image
+        g.drawImage(testImage, 0, 0, width, height,this);
     }
 
     //  Main method required to initialize the app
